79d4cce6a8fcbb95598cf1ac539ac20e3f92c5c8
HarshVardhan <143250140+1FPSLYF@users.noreply.github.com>
2024-07-31 15:55:39 +0530

    host ip change

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const Host = "0.0.0.0";
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
 app.use(
   cors({
     origin: "*",
@@ -14,12 +15,12 @@ app.use(
 
 app.post("/", async (req, res) => {
   const { url } = req.body;
-  console.log("url  ",url)
+  console.log("url  ", url);
   const response = await axios.get(url);
-  
+
   res.send(response.data);
 });
 
-app.listen(port, () => {
+app.listen(port,Host, () => {
   console.log(`Example app listening on port ${port}`);
 });
