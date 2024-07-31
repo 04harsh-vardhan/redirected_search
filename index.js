726479bc31a0cbfdd4e2dd37bf9d223499939f08
HarshVardhan <143250140+1FPSLYF@users.noreply.github.com>
2024-07-31 15:49:38 +0530

    changes

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.post("/", async (req, res) => {
   const { url } = req.body;
-  
+  console.log("url  ",url)
   const response = await axios.get(url);
+  
   res.send(response.data);
 });
 
